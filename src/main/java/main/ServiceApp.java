5d47d3f8ac0a83d56fc2475453a44deed61cb50d
Artur Khusnullin <artur97hus@gmail.com>
2018-01-30 18:45:39 +0200

    First

---
+++ src/main/java/main/ServiceApp.java
@@ -0,0 +1,14 @@
+package main;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by roshanalwis on 9/30/17.
+ */
+
+@SpringBootApplication
+public class ServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApp.class, args);
+    }
+}
